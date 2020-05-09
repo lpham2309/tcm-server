832d21619d60aafeb343291087fa5b3d4cc51f69
Lam Pham <lam.pham3110@gmail.com>
2020-05-08 23:06:21 -0400

    heroku deploy

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ class ItemList(Resource):
     #     connection.close()
 
 
-api.add_resource(ItemList, '/items')
+api.add_resource(ItemList, '/')
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=8000, debug=True)
