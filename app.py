7ad499ea4d8a759c8a4b69d03fa251973eca603e
Lam Pham <lam.pham3110@gmail.com>
2020-05-08 22:32:45 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import mysql.connector
+
+config = {
+    'user': 'root',
+    'password': 'password',
+    'host': 'db',
+    'port': '3306',
+    'database': 'items'
+}
+
+
+app = Flask(__name__)
+api = Api(app)
+
+items = [
+    {
+        'name': 'Lam',
+        'grade': 10
+    },
+    {
+        'name': 'Tester',
+        'grade': 3
+    }
+]
+
+class ItemList(Resource):
+    def get(self):
+        return items
+
+    # def post(self):
+    #     connection = mysql.connector.connect(**config)
+    #     cursor = connection.cursor()
+    #     cursor.execute('SELECT * FROM items')
+    #     results = [{name: item} for (name, grade) in cursor]
+    #     cursor.close()
+    #     connection.close()
+
+
+api.add_resource(ItemList, '/items')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
